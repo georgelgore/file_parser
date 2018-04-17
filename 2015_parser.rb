d25bfc848e8b85d0fe3659840e519b274b9051c9
George Gore <georgelgore@gmail.com>
2018-04-17 19:35:37 -0400

    add price to works array

--- 2015_parser.rb
+++ 2015_parser.rb
@@ -3,14 +3,14 @@ require 'nokogiri'
 require 'json'
 
 lot_file_names = Dir["./data/2015-03-18/*"]
-artists= []
+lots= []
 
 lot_file_names.each do |file_name|
   doc = File.open(file_name) { |f| Nokogiri::HTML(f) }
-  name = doc.xpath('//h2').children.to_s.gsub(/\(.*\)/, "")
-  artwork = doc.xpath('//h3').children.first.to_s
-  # artists_names << name unless artists_names.include?(name)
-  artists << {name: name, works: [artwork]}.to_json
+  artist_name = doc.xpath('//h2').children.to_s.gsub(/\(.*\)/, "")
+  artwork_title = doc.xpath('//h3').children.first.to_s
+  lot_cost = doc.xpath('//div').children.last.to_s
+  lots << {name: artist_name, works: [{title: artwork_title, price:lot_cost }]}.to_json
 end
 
-p artists
+puts lots
