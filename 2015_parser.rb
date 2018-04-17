cf34acca07c276bc5e8aac4ab3aceebee55b8335
George Gore <georgelgore@gmail.com>
2018-04-17 19:29:13 -0400

    add array or art works

--- 2015_parser.rb
+++ 2015_parser.rb
@@ -3,12 +3,14 @@ require 'nokogiri'
 require 'json'
 
 lot_file_names = Dir["./data/2015-03-18/*"]
-artists_names = []
+artists= []
 
 lot_file_names.each do |file_name|
   doc = File.open(file_name) { |f| Nokogiri::HTML(f) }
   name = doc.xpath('//h2').children.to_s.gsub(/\(.*\)/, "")
-  artists_names << name unless artists_names.include?(name)
+  artwork = doc.xpath('//h3').children.first.to_s
+  # artists_names << name unless artists_names.include?(name)
+  artists << {name: name, works: [artwork]}.to_json
 end
 
-puts artists_names.to_json
+p artists
