4770e4dd3c770fb69c5431e0f1264a609d3d8a74
George Gore <georgelgore@gmail.com>
2018-04-17 19:47:36 -0400

    add currency and amount seperatly

--- 2015_parser.rb
+++ 2015_parser.rb
@@ -3,14 +3,15 @@ require 'nokogiri'
 require 'json'
 
 lot_file_names = Dir["./data/2015-03-18/*"]
-lots= []
+lots = []
 
 lot_file_names.each do |file_name|
   doc = File.open(file_name) { |f| Nokogiri::HTML(f) }
   artist_name = doc.xpath('//h2').children.to_s.gsub(/\(.*\)/, "")
   artwork_title = doc.xpath('//h3').children.first.to_s
-  lot_cost = doc.xpath('//div').children.last.to_s
-  lots << {name: artist_name, works: [{title: artwork_title, price:lot_cost }]}.to_json
+  lot_cost = doc.xpath('//div').children.last.to_s.split(" ")
+
+  lots << {name: artist_name, works: [{title: artwork_title, currency:lot_cost.first, amount: lot_cost.last }]}.to_json
 end
 
 puts lots
